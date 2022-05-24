ee5357326397576e3340145ab10eae2e0ae59989
SmitchyB <bresmitc@uat.edu>
2022-05-23 19:01:40 -0700

    Options Menu
    List of changes:
    Options Menu is no longer a scene by itself and has been moved as a different canvas.
    Sliders: Now hold the position of the volumes assigned to it.
    Music no longer restarts ever time you click on options and then back to main menu.

--- Assets/Scripts/Menu, GM, & UI Scripts/StartMenuFuctions.cs
+++ Assets/Scripts/Menu, GM, & UI Scripts/StartMenuFuctions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class StartMenuFuctions : MonoBehaviour
 {
@@ -14,12 +15,37 @@ public class StartMenuFuctions : MonoBehaviour
     //Button to load the Options Menu Scene
     public void Options_Button()
     {
-        SceneManager.LoadScene("Options Menu");
+        
     }
-
     //Button to quit the game from the Main Menu
     public void Quit_Button()
     {
         Application.Quit();
     }
+
+    public AudioMixer mixer; //gets the audio mixer from unity
+
+
+    public void SetMaster(float Master) //master volume mixer slider
+    {
+
+        mixer.SetFloat("Master Volume", Master);
+
+    }
+
+    public void SetMusic(float Music) //music volume mixer slider
+    {
+
+        mixer.SetFloat("Music Volume", Music);
+
+    }
+
+    public void SetSFX(float SFX) //SFX volume mixer slider
+    {
+
+        mixer.SetFloat("SFX Volume", SFX);
+
+    }
+
+
 }
